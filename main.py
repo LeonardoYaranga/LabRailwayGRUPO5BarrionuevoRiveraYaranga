735db75680a521343073232eb28efe062cc59215
LeonardoYaranga <ljyaranga@espe.edu.ec>
2024-06-07 02:37:08 -0500

    AdvancedFlask

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,render_template,request
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    my_set = {"Hello World, our names are Dome, Alesso and Leo!"}
-    return jsonify(list(my_set))
+    ####Only to show a jsonify message
+    #my_set = {"Hello World, our names are Dome, Alesso and Leo!"}
+    #return jsonify(list(my_set))
+    ###Show a template
+    return render_template('index.html')
 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/api')
+def get_data():
+    data = {
+        "names": ["Dome", "Alesso", "Leo"]
+    }
+    return jsonify(data)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name= request.form['name']
+
+    return jsonify({"name":name})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
